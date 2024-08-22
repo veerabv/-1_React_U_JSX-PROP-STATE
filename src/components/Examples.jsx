2f4a45fe19e10b7b1f1adc841b9c97c7c06c20e9
veerabv <140704580+veerabv@users.noreply.github.com>
2024-08-22 20:27:04 +0530

    Update Examples.jsx

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -13,7 +13,7 @@ function Examples() {
     <>
       <Section id="examples" title="Examples">
         <Tabs
-         buttonsContainer="menu"
+         ButtonsContainer="menu"
           buttons={
             <>
               <TabButton
